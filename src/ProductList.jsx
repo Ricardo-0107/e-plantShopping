c77236da092b79c8029c30e135b1e6685330d5ed
Ricardo_0107 <ricardososamoreno37@gmail.com>
2026-06-12 23:10:54 -0400

    Complete Task 3 CartItem component

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addItem } from './CartSlice';
 import './ProductList.css'
 import CartItem from './CartItem';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
 const [addedToCart, setAddedToCart] = useState({});
+const dispatch = useDispatch();
 const handleAddToCart = (product) => {
   dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
 
@@ -298,7 +301,7 @@ const handleAddToCart = (product) => {
           <div className="product-title">{plant.name}</div> {/* Display plant name */}
           {/* Display other plant details like description and cost */}
           <div className="product-description">{plant.description}</div> {/* Display plant description */}
-          <div className="product-cost">${plant.cost}</div> {/* Display plant cost */}
+          <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
           <button
             className="product-button"
             onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
